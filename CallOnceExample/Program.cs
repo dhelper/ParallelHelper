ffdd8ae65b6eac1e2b40e9eb1bc38eafb56c0281
dhelper <dror.helper@gmail.com>
2014-02-24 22:13:05 +0200

    Added Examples for Once

---
+++ CallOnceExample/Program.cs
@@ -0,0 +1,37 @@
+﻿using ParallelHelper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CallOnceExample
+{
+    class Program
+    {
+        static OnceFlag _flag = new OnceFlag();
+        static void Main(string[] args)
+        {
+            var t1 = new Thread(() => DoOnce(1));
+            var t2 = new Thread(() => DoOnce(2));
+            var t3 = new Thread(() => DoOnce(3));
+            var t4 = new Thread(() => DoOnce(4));
+
+            t1.Start();
+            t2.Start();
+            t3.Start();
+            t4.Start();
+
+            t1.Join();
+            t2.Join();
+            t3.Join();
+            t4.Join();
+        }
+
+        private static void DoOnce(int index)
+        {
+            Call.Once(_flag, () => Console.WriteLine("Callled once (" + index + ")"));
+        }
+    }
+}
